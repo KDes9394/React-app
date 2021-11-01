dccb656f292c6fb9e99a38d2fbce047773dfb9c5
lasj <kentousa@gmail.com>
2021-10-31 22:57:19 -0700

    first

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products:[
+        {
+            id: '1',
+            name: 'Raspberry Pi',
+            price: 14,
+            image: 'https://images.unsplash.com/photo-1560180474-e8563fd75bab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+        },
+        {
+            id: '2',
+            name: 'Lemon Tart',
+            price: 12,
+            image: 'https://images.unsplash.com/photo-1525151498231-bc059cfafa2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1160&q=80'
+        },
+        {
+            id: '3',
+            name: 'Apple Pie',
+            price: 10,
+            image: 'https://images.unsplash.com/photo-1625833196424-a1be6544e069?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80I'
+        }
+    ]
+};
+
+export default data;
